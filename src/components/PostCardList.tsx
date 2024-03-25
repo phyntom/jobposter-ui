d18e9804db44462e80b3d3c73481cba282bb2b79
Aimable Ruhumuliza <aimable@Aimables-MacBook-Pro-3.local>
2024-03-26 01:17:48 +0300

    adding react-query to fetch and cache the data

--- src/components/PostCardList.tsx
+++ src/components/PostCardList.tsx
@@ -1,31 +1,25 @@
-import { useEffect, useState } from 'react';
 import PostCard from './PostCard';
 import { JobPost } from '../schema';
 import Spinner from './Spinner';
+import { useQuery } from 'react-query';
 
 type PostCardListProps = {
   isHome?: boolean;
 };
 
+const fetchJobs = async (isHome: boolean) => {
+  const apiURL = isHome ? '/api/jobs?_limit=6' : '/api/jobs';
+  const res = await fetch(apiURL);
+  return res.json();
+};
+
 const PostCardList = ({ isHome = false }: PostCardListProps) => {
-  const [jobs, setJobs] = useState<JobPost[]>([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const fetchJobs = async () => {
-      const apiURL = isHome ? '/api/jobs?_limit=6' : '/api/jobs';
-      try {
-        const response = await fetch(apiURL);
-        const data = await response.json();
-        setLoading(false);
-        setJobs(data);
-      } catch (err) {
-        console.error('Fetch Jobs Error:', err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchJobs();
-  }, [isHome]);
+  const { isLoading, error, data } = useQuery<JobPost[], Error>('jobs', () =>
+    fetchJobs(isHome)
+  );
+  if (isLoading) return <Spinner loading={isLoading} />;
+
+  if (error) return <div>Something went wrong...</div>;
 
   return (
     <div>
@@ -34,15 +28,11 @@ const PostCardList = ({ isHome = false }: PostCardListProps) => {
           <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
             {isHome ? 'Browse Jobs' : 'Jobs Listing'}
           </h2>
-          {loading ? (
-            <Spinner loading={loading} />
-          ) : (
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:md:grid-cols-2 xl:grid-cols-3 gap-6'>
-              {jobs?.map((jobPost) => (
-                <PostCard key={jobPost.id} jobPost={jobPost} />
-              ))}
-            </div>
-          )}
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:md:grid-cols-2 xl:grid-cols-3 gap-6'>
+            {data?.map((jobPost) => (
+              <PostCard key={jobPost.id} jobPost={jobPost} />
+            ))}
+          </div>
         </div>
       </section>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { NextUIProvider } from '@nextui-org/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <main className={`text-foreground h-screen font-poppins`}>
-        <App />
-      </main>
+      <QueryClientProvider client={queryClient}>
+        <main className={`text-foreground h-screen font-poppins`}>
+          <App />
+        </main>
+      </QueryClientProvider>
     </NextUIProvider>
   </React.StrictMode>
 );
