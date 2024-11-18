576eb34618260be01ec3c7ad2fbc0baed48544c2
Aimable Ruhumuliza <phyntom@gmail.com>
2024-11-18 00:41:10 -0500

    adding accepting public and privage ips

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-
-
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
    const env = loadEnv(mode, process.cwd(), '');
    return {
       plugins: [react()],
@@ -13,6 +11,7 @@ export default defineConfig(({ command, mode }) => {
       },
       base: '/', // Set base to root for correct asset paths in production
       server: {
+         host: true,
          port: env.PORT,
          proxy:
             env.NODE_ENV === 'development'
